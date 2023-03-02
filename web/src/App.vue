0b6fea07b15995a2f540f7919e282055b041e65b
AndyZhao5652 <54948555@qq.com>
2023-03-02 16:59:50 +0800

    4-7 制作Vue自定义组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
+
+<script lang="ts">
+
+  import {defineComponent} from "vue";
+  import TheHeader from '@/components/the-header.vue';
+  import TheFooter from '@/components/the-footer.vue';
+
+  export default defineComponent({
+    name: 'app',
+    components:{
+      TheHeader,
+      TheFooter,
+    },
+  });
+</script>
